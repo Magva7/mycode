52457a8a4d9d15efd7fbb4a7329201c4beb009af
Magva7 <magva7@gmail.com>
2019-12-29 00:07:32 +0300

    add

--- src/dz_GMByte/dz1_GMByte.java
+++ src/dz_GMByte/dz1_GMByte.java
@@ -1,8 +1,8 @@
-/**Вывод строки 5 раз*/
 package dz_GMByte;
 
 public class dz1_GMByte {
     public static void main(String[] args) {
+        /**Вывод строки 5 раз*/
         for(int i=0;i<5;i++){
             System.out.print("Ура! Сделан мой первый шаг в java! ");
         }
