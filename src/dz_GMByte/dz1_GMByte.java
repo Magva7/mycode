f74b0c72334b8cc71a0f8f5634e51144ca64be45
Magva7 <magva7@gmail.com>
2019-12-29 00:05:30 +0300

    add dz

---
+++ src/dz_GMByte/dz1_GMByte.java
@@ -0,0 +1,9 @@
+package dz_GMByte;
+
+public class dz1_GMByte {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            System.out.print("Ура! Сделан мой первый шаг в java! ");
+        }
+    }
+}
